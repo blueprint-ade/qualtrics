a6c28fb9968eccc084a652a3b0e806f121677711
blueprint-ade <tmcmanus@blueprint-ade.ca>
2018-03-12 11:03:33 -0400

    Update survey_utils.js

--- survey_utils.js
+++ survey_utils.js
@@ -58,7 +58,7 @@ function embed_gender_sf(q_obj, key) {
 		
 		console.log(q_retrieve(key));
 	
-	})
+	});
 
 }
 
@@ -77,7 +77,7 @@ function embed_text_exists(q_obj, key) {
 		}
 		console.log(q_retrieve(key));
 	
-	})
+	});
 
 }
 
@@ -108,29 +108,6 @@ function select_all_boolean(q_obj) {
 		console.log("embedded values", response_array);
 		
 	});  
-	
-}
-
-
-function calendar_picker(q_obj) {
-	
-	When did you start this job? <i>If you don't know the exact day, please give your best guess.</i><br><br>
-
-<link href="https://ajax.googleapis.com/ajax/libs/yui/2.9.0/build/calendar/assets/skins/sam/calendar.css" rel="stylesheet" type="text/css"/><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/yui/2.9.0/build/yahoo-dom-event/yahoo-dom-event.js"></script> <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/yui/2.9.0/build/calendar/calendar-min.js"></script> <script>Qualtrics.SurveyEngine.addOnload(function(){var qid =this.questionId;var calid = qid +'_cal';var y =QBuilder('div');
-  $(y).setStyle({clear:'both'});var d =QBuilder('div',{className:'yui-skin-sam'},[QBuilder('div',{id:calid}),
-    y
-  ]);var c =this.questionContainer;
-  c = $(c).down('.QuestionText');
-  c.appendChild(d);var cal1 =new YAHOO.widget.Calendar(calid); 
-  cal1.render();var input = $('QR~'+ qid);
-  $(input).setStyle({marginTop:'20px',width:'150px'});var p =$(input).up();var x =QBuilder('div');
-  $(x).setStyle({clear:'both'});
-  p.insert(x,{position:'before'});
-    cal1.selectEvent.subscribe(function(e,dates){var date = dates[0][0];if(date[1]<10)
-        date[1]='0'+ date[1];if(date[2]<10)
-        date[2]='0'+ date[2];
-  
-    input.value = date[2]+'/'+date[1]+'/'+date[0];})});</script>
 	
 }
 
